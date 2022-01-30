1eea9be0337499eeedaedb66f46ec504df688947
Brian Pfeil <brian.pfeil@gmail.com>
2022-01-29 20:58:21 -0500

    added exec env

--- aws-glue-sam-example/my-glue-script-01.py
+++ aws-glue-sam-example/my-glue-script-01.py
@@ -6,14 +6,19 @@ import subprocess
 # printing environment variables
 print(os.environ)
 
+result = subprocess.run(['aws', '--version'], stdout=subprocess.PIPE)
+print(result.stdout.decode("utf-8"))
+
+
 result = subprocess.run(['aws', 'sts', 'get-caller-identity'], stdout=subprocess.PIPE)
 print(result.stdout.decode("utf-8"))
 
+result = subprocess.run(['find', '.'], stdout=subprocess.PIPE)
+print(result.stdout.decode("utf-8"))
 
 for directory_path in os.environ['PATH'].split(":"):
     result = subprocess.run(['find', directory_path], stdout=subprocess.PIPE)
     print(result.stdout.decode("utf-8"))
-
 
 result = subprocess.run(['curl', '-s', 'https://www.google.com', '-o', 'response.log'], stdout=subprocess.PIPE)
 result = subprocess.run(['cat', 'response.log'], stdout=subprocess.PIPE)
